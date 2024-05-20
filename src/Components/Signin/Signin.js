deef50f2b9e40f248a6c57f596da62d7ae33712e
azeekk <azeekkalathil99@gmail.com>
2024-05-20 23:39:44 +0530

    acount creation ready

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,13 +1,18 @@
+import { useState } from 'react'
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import './Signin.css'
 import { Firebase } from '../../Firebase/firebase'
 
 function Signin() {
+const [firstName,setFirstName] = useState('');
+const [secondName,setSecondName] = useState('');
+const [email,setEmail] = useState('');
+const [password,setPassword] = useState('')
+
 function handleSubmit(e) {
   e.preventDefault()
-  Firebase.auth().createUserWithEmailAndPassword().then((userCredentials)=>{
-    console.log(userCredentials);
+  Firebase.auth().createUserWithEmailAndPassword(email,password).then(()=>{
   })
 }
   return (
@@ -16,14 +21,43 @@ function handleSubmit(e) {
       <div className='container'>
         <form>
             <label>first-name</label>
-            <input type="text" name='first-name' className='first' placeholder='firstname' />
+
+            <input type="text"
+              name='first-name'
+              value={firstName}
+              onChange={event =>setFirstName(event.target.value)}
+              className='first'
+              placeholder='firstname' />
+
             <label>second-name</label>
-            <input type="text" name='second-name' className='second' placeholder='secondname'/>
+
+            <input type="text"
+              name='second-name'
+              value={secondName}
+              onChange={event => setSecondName(event.target.value)}
+              className='second'
+              placeholder='secondname'/>
+
             <label>email</label>
-            <input type="email" name='email' className='email' placeholder='email' />
+
+            <input type="email"
+              name='email'
+              value={email}
+              onChange={event => setEmail(event.target.value)}
+              className='email'
+              placeholder='email' />
+
             <label>password</label>
-            <input type="password" name='password' className='password' placeholder='password' />
+
+            <input type="password"
+             name='password'
+             value={password}
+             onChange={event => setPassword(event.target.value)}
+              className='password'
+               placeholder='password' />
+
             <button onClick={handleSubmit}>Submit</button>
+
         </form>
       </div>
     </div>
